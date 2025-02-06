af321e0fe4759aafb55ca871fe869ee34f692793
Akancha Kumari <akxa75041@gmail.com>
2025-02-06 15:30:10 +0200

    modified

--- Level2/ft_atoi.c
+++ Level2/ft_atoi.c
@@ -4,10 +4,10 @@
 int	ft_atoi(const char *str)
 {
 	int i = 0;
-	int sign;
+	int sign = 1;
 	long result;
 
-	while(str[i] != '\0' && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
+	while(str[i] != '\0' && (str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
 	if(str[i] != '\0' && (str[i] == '-' || str[i] == '+'))
 	{
@@ -15,7 +15,7 @@ int	ft_atoi(const char *str)
 			sign = -sign;
 		i++;
 	}
-	while(str[i] > '0' && str[i] < '9')
+	while(str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 + str[i] - '0';
 		i++;
@@ -25,7 +25,7 @@ int	ft_atoi(const char *str)
 
 int main(void)
 {
-	char *str = "-65923fdgfegg6408523";
+	char *str = "-6536734653dgfegg6408523";
 	printf("%d\n",ft_atoi(str));
 	printf("%d\n", atoi(str));
-}
+}#include <stdio.h>
